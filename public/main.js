83b06b7bfd860a3fbc24a1e93f878f676e0573eb
Tomoki Aonuma <uasi@uasi.jp>
2014-10-25 20:08:27 +0900

    Fix main.js

--- public/main.js
+++ public/main.js
@@ -1,9 +1,10 @@
 function startSlideshow() {
   var path = location.pathname;
   if (path == '/') return;
+  path += '.md';
   $.get(path, function(data, stat) {
-      $('#source').get(0).innerText = data;
-      var slideshow = remark.create();
+    $('#source').get(0).innerText = data;
+    var slideshow = remark.create();
   });
 }
 
